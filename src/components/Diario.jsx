c7b87d3284f88dd3e56270b384c6f62f1b4c85f2
Sasa Savic Perusina <sasa@bubulazy.com>
2022-12-16 14:49:55 +0000

    Trabajando en props de la tarjeta, css terminado

--- src/components/Diario.jsx
+++ src/components/Diario.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Card from './Card'
 
 export default function Diario() {
   return (
-    <h1>Diario</h1>
+    <div className='container'>
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
+
+
+// title: "Mount Fuji",
+// location: "Japan",
+// googleMapsUrl: "https://duckduckgo.com/?q=mount+fuji&va=b&t=hc&ia=web&iax=about&iaxm=maps&strict_bbox=0&bbox=35.37794632426973%2C138.70331388320457%2C35.353911297345476%2C138.7487744745163",
+// startDate: "12 Jan, 2021",
+// endDate: "24 Jan, 2021",
+// description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+// imageUrl: "https://source.unsplash.com/WLxQvbMyfas/w=600"
